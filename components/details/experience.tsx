9c95df64475560bf5f04ffcd11da1fc8054940d0
terminalwarlord <joybiswas389@gmail.com>
2025-03-24 01:22:44 +0600

    reusable card added

--- components/details/experience.tsx
+++ components/details/experience.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import ExperienceCard from './experience-card'
+import Card from './card'
 
 const Experience = () => {
   return (
     <div className='w-full' id='experience'>
-      <ExperienceCard
-        from='2024'
+      <Card
+        duration='2024 - Present'
         role='Senior Software engineer'
         description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae deserunt impedit placeat beatae, soluta quod cumque ipsum nobis harum, quos excepturi autem ea. Aliquid, incidunt. Error facilis libero accusamus doloremque dolor quisquam omnis placeat! Eum iure quaerat, error impedit est molestias labore dolor corrupti nemo, esse, earum aut eligendi!'
         stacks={['Typescript', 'React', 'Node', 'Prisma', 'Prisma']}
       />
-      <ExperienceCard
-        from='2024'
+      <Card
+        duration='2024 - 2025'
         role='Senior Software engineer'
         description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae deserunt impedit placeat beatae, soluta quod cumque ipsum nobis harum, quos excepturi autem ea. Aliquid, incidunt. Error facilis libero accusamus doloremque dolor quisquam omnis placeat! Eum iure quaerat, error impedit est molestias labore dolor corrupti nemo, esse, earum aut eligendi!'
         stacks={['Typescript', 'React', 'Node', 'Prisma', 'Prisma']}
--- components/details/projects.tsx
+++ components/details/projects.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import ProjectCard from './project-card';
+import Card from './card';
 
 const Projects = () => {
   return (
     <div id='projects' className=''>
-        <ProjectCard/>
+      <Card
+        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dignissimos accusantium quod. Cumque voluptatum a, quibusdam nam iste, officia tempora odit sequi sit hic sapiente perferendis assumenda, harum ut molestias.'
+        imagePath='/images/contest-tracker.png'
+        role='Contest Tracker'
+        links={[
+          {
+            title: 'Demo',
+            url: 'https://contest-tracker.joybiswas.com/'
+          },
+          {
+            title: 'Github',
+            url: 'https://github.com/TerminalWarlord/TLE-Contest-Tracker'
+          }
+        ]}
+        stacks={['React', 'Node', 'Mongoose', 'Express']}
+
+      />
+
     </div>
   )
 }
